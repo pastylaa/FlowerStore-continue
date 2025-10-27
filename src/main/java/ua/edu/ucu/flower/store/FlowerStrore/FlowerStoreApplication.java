03f07d3f3c13873b341e880b2e055213dfb2d578
Pasternak <yuliya.pasternak.2007@gmail.com>
2025-10-27 23:29:50 +0200

    change name

--- src/main/java/ua/edu/ucu/flower/store/FlowerStrore/FlowerStoreApplication.java
+++ src/main/java/ua/edu/ucu/flower/store/FlowerStrore/FlowerStoreApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlowerStroreApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FlowerStroreApplication.class, args);
-	}
-
+public class FlowerStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerStoreApplication.class, args);
+    }
 }
